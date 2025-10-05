fee510c76d06f25b65188aec459306e489330605
d Kong <dkong_69@yahoo.com>
2025-10-05 16:45:04 -0500

    00-00

--- WillowWeb/vite.config.ts
+++ WillowWeb/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/WillowWeb/', // 👈 cambia "WillowWeb" por el nombre EXACTO de tu repo
 })
